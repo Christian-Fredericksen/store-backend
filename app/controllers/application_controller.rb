5439f6895d26d49090234997d848ae228a95d9d9
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-09 14:03:45 -0800

    def User.create and added current_user to AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+    def current_user 
+        current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    
     def current_cart
         current_cart ||= Cart.find(session[:cart_id]) if session[:cart_id]
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,27 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
-
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
+  def create 
+    user = User.create(user_params)
+    cart = Cart.create
+    cart.user = user
+    session[:user_id] = user.id
+    session[:cart_id] = cart.id
+    cookies[:cart_id] = cart.id
+    
+    render json: {
+      logged_in: true,
+      user: current_user,
+      cart: {
+        id: current_cart.id,
+        items: current_cart.items,
+        total: current_cart_total
+      }
+    }
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
-    end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
 end
