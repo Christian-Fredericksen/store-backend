f36b898297977b8199d3969404229185dfff16e0
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-09 12:09:48 -0800

    defined Rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :carts
-  resources :items
-  resources :users
+  post '/users', to: 'users#create'
+  delete '/logout', to: 'sessions#logout'
+  post '/login', to: 'sessions#create'
+  get '/items', to: 'items#index'
+  
+  post '/carts', to: 'carts#add_item_to_cart'
+  post '/orders', to: 'orders#create'
+
+  delete '/carts/:cart_id', to: 'carts#remove_item_from_cart'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
