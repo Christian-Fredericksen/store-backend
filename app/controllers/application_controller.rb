70f29ab553401ff0afa385c8ae4dd9f36f38518a
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-09 13:40:55 -0800

    added current_cart & current_cart_total actions to AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    def current_cart
+        current_cart ||= Cart.find(session[:cart_id]) if session[:cart_id]
+    end
+
+    def current_cart_total
+        item_prices = current_cart.items.map { |i| i.price }
+        item_prices.reduce(0) { |sum, price| sum + price }
+    end
+    
 end
