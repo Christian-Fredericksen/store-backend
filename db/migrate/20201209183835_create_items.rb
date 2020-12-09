0e82cb33f105094f6df22d49f65ad8016c3f8ce7
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-12-09 10:39:14 -0800

    gen scaffold for Item

---
+++ db/migrate/20201209183835_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :description
+      t.string :image
+      t.string :price
+
+      t.timestamps
+    end
+  end
+end
